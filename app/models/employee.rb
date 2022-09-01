c3799f212128484736a5ad0d1c3746f1e5ed3efa
abhay0806 <sharmaabhay797@gmail.com>
2022-09-01 18:26:52 -0400

    intendation fix

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,8 +2,7 @@ class Employee < ApplicationRecord
 
   has_many :admins
 
-
-	validates :name, presence: true
+  validates :name, presence: true
   validates :employee_detail, presence: true
   validates :date, presence: true
   validates :is_admin, presence: true
